004ca6d3ead3392b89b82c6fa2d97c1deb8aef68
baridian <ianukusa@gmail.com>
2018-08-01 00:01:42 -0500

    added newTree function, fixed branch error

---
+++ src/data structures/tree.h
@@ -0,0 +1,37 @@
+#ifndef CRCT_TREE_H
+#define CRCT_TREE_H
+
+#endif //CRCT_TREE_H
+
+typedef struct treeNode treeNode;
+
+struct treeNode
+{
+    treeNode *left;
+    treeNode *right;
+    treeNode *parent;
+    void *data;
+};
+
+typedef struct tree
+{
+    treeNode *root;
+    unsigned dataSize;
+    treeNode *current;
+} tree;
+
+typedef enum treeDir{left, right, up} treeDir;
+
+tree newTree(int);
+
+void addNode(tree,treeDir,void *);
+
+void step(tree *,treeDir);
+
+void splice(tree,treeDir,tree);
+
+void trim(tree,treeDir);
+
+void stepToLowestInternal(tree *);
+
+void freeTree(tree);
